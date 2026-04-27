1773cdf629918059d9b423de20ca88996887a286
SofiaTilde <sofia.tilde@gmail.com>
2026-04-27 16:40:10 +0200

    Add bullet files

--- chai3d-3.3.0/src/version.h
+++ chai3d-3.3.0/src/version.h
@@ -1,10 +1,19 @@
-// version info                                   
-#define CHAI3D_DESCRIPTION     "Haptic Simulation Framework"  
-#define CHAI3D_VERSION_MAJOR   3          
-#define CHAI3D_VERSION_MINOR   3          
-#define CHAI3D_VERSION_PATCH   0          
-#define CHAI3D_PRERELEASE      ""   
-#define CHAI3D_METADATA        "+g9f2e2b0b2"     
-#define CHAI3D_VERSION         "3.3.0"      
-#define CHAI3D_VERSION_FULL    "3.3.0+g9f2e2b0b2" 
-#define CHAI3D_REVISION        0x9f2e2b0b2               
+// version info
+#define CHAI3D_DESCRIPTION "Haptic Simulation Framework"
+#define CHAI3D_VERSION_MAJOR 3
+#define CHAI3D_VERSION_MINOR 3
+#define CHAI3D_VERSION_PATCH 0
+#define CHAI3D_PRERELEASE ""
+#define CHAI3D_METADATA "+g9f2e2b0b2"
+#define CHAI3D_VERSION "3.3.0"
+#define CHAI3D_VERSION_FULL "3.3.0+g9f2e2b0b2"
+#define CHAI3D_REVISION 0x9f2e2b0b2
+#define CHAI3D_BULLET_DESCRIPTION "Bullet wrapper module for CHAI3D"
+#define CHAI3D_BULLET_VERSION_MAJOR 1
+#define CHAI3D_BULLET_VERSION_MINOR 0
+#define CHAI3D_BULLET_VERSION_PATCH 0
+#define CHAI3D_BULLET_PRERELEASE ""
+#define CHAI3D_BULLET_METADATA "+g9f2e2b0b2"
+#define CHAI3D_BULLET_VERSION "1.0.0"
+#define CHAI3D_BULLET_VERSION_FULL "1.0.0+g9f2e2b0b2"
+#define CHAI3D_BULLET_REVISION 0x9f2e2b0b2
